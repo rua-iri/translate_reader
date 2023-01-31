3ba83d98d05367f4c27c56a3fc6fe6b19f4d4fa2
rua-iri <rory.mcguigan@protonmail.com>
2023-01-31 18:53:40 +0000

    add function activated by clicking the arrow buttons at the sides

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -6,9 +6,12 @@ export default function Definition(props) {
     //TODO give this good layout with phonetic spelling, meaning, tense, root and verbForm
     //these should all be passed to the component via props
 
+    // TODO maybe upperText variable is no longer necessary
+
     // TODO add a means by which the user can cycle through all possible meanings by clicking a button
     const [upperText, setUpperText] = React.useState();
     const [possibleMeanings, setPossibleMeanings] = React.useState([]);
+    const [resultCounter, setResultCounter] = React.useState(0);
 
 
     const makeAPICall = async (apiLink) => {
@@ -19,6 +22,8 @@ export default function Definition(props) {
             data.forEach(dat => {
                 console.log({ dat })
             });
+
+            // TODO set number of results to a variable to use later
 
         }
         catch (e) {
@@ -39,6 +44,30 @@ export default function Definition(props) {
     }, [props]);
 
 
+
+    // function to cycle to previous method
+    function prevResult() {
+        console.log("Previous");
+
+        if(resultCounter>0) {
+            setResultCounter(resultCounter-1);
+            console.log(resultCounter);
+        }
+        
+    }
+
+    // function to cycle to previous method
+    function nextResult() {
+        console.log("Next");
+
+
+        if(resultCounter<possibleMeanings.length) {
+            setResultCounter(resultCounter+1);
+            console.log(resultCounter);
+        }
+    }
+
+
     // TODO add an onclick event to the definition arrows so that they will cycle to the next meaning
     
     // TODO give the definition arrows conditional rendering so that they only show if there is a next/previous definition
@@ -46,7 +75,7 @@ export default function Definition(props) {
 
     return (
         <div className="top-banner gimme-outline">
-            <div className="gimme-outline definition-arrow">
+            <div className="gimme-outline definition-arrow" onClick={prevResult}>
                 {">"}
             </div>
             <div className="selected-word arab-text gimme-outline">
@@ -55,7 +84,7 @@ export default function Definition(props) {
             <div className="translations gimme-outline">
                 <Translation allTranslations={possibleMeanings} />
             </div>
-            <div className="gimme-outline definition-arrow">
+            <div className="gimme-outline definition-arrow" onClick={nextResult}>
                 {"<"}
             </div>
         </div>
