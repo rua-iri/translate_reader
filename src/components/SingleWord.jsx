f9722c97f7b4a43ab671193fb223516098b9b7a6
rua-iri <rory.mcguigan@protonmail.com>
2024-10-02 22:45:21 +0300

    fix: text layout issue
    resolves #5

---
+++ src/components/SingleWord.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function SingleWord({ isSelected, onClick, alt, wordContent }) {
+  let fontWeight = "";
+
+  if (isSelected) {
+    fontWeight = "bg-slate-200  rounded";
+  }
+
+  if (!wordContent) {
+    return <div className="h-3 basis-full"></div>;
+  } else {
+    return (
+      <span
+        dir="rtl"
+        className={`cursor-pointer ${fontWeight}`}
+        onClick={() => onClick(alt)}
+      >
+        {wordContent}
+        &nbsp;
+      </span>
+    );
+  }
+}
