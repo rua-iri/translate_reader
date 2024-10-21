93a22a9d9e456770a7e799ac6c554498e20adbf6
rua-iri <rory.mcguigan@protonmail.com>
2024-10-21 22:15:47 +0300

    update: API fetch

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,7 +1,13 @@
 const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 
-
 export async function fetchWordMeanings(word) {
-  const response = await fetch(`${BASE_API_URL}/word/${word}`);
-  return await response.json();
+  try {
+    const response = await fetch(`${BASE_API_URL}/word/${word}`);
+    const data = await response.json();
+
+    return data.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
