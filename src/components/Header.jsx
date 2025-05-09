c11931d4b23829d962eb3ebe240f89c0d034a982
rua-iri <rory.mcguigan@protonmail.com>
2025-05-09 21:00:28 +0300

    add: page header

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex rounded-b-lg py-3 mb-2 lg:mb-10 flex-row justify-center w-full bg-slate-600 text-slate-50">
+      <p className="text-xl">Translate Reader</p>
+    </div>
+  );
+}
